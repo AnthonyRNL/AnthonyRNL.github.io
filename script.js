1047842f56f4922be6a0811948b7c343ec64feb6
github username <github email address>
2015-07-20 13:33:42 -0400

    working structure, still need to sort out hardmode bugs

--- script.js
+++ script.js
@@ -1,53 +1,79 @@
 //board setup
 var a = ["_", "_","_","_", "_","_","_", "_","_"]
-// var diagL = [a[0], a[4], a[8]]
-// var diagR =  [a[2], a[4], a[6]]
-// var column1 = [a[0], a[3], a[6]]
-// var column2 = [a[1], a[4], a[7]]
-// var column3 = [a[2], a[5], a[8]]
-// var row1 = [a[0], a[1], a[2]]
-// var row2 = [a[3], a[4], a[5]]
-// var row3 = [a[6], a[7], a[8]]
-// var winStates = [diagL, diagR, column1, column2, column3, row1, row2, row3]
+
+var checkBlocks = function(){
+  var diagL = [[a[0], 0], [a[4], 4], [a[8], 8]]
+  var diagR =  [[a[2],2], [a[4],4], [a[6],6]]
+  var column1 = [[a[0],0], [a[3],3], [a[6],6]]
+  var column2 = [[a[1],1], [a[4],4], [a[7],7]]
+  var column3 = [[a[2],2], [a[5],5], [a[8],8]]
+  var row1 = [[a[0],0], [a[1],1], [a[2],2]]
+  var row2 = [[a[3],3], [a[4],4], [a[5],5]]
+  var row3 = [[a[6],6], [a[7],7], [a[8],8]]
+  var winStates = [diagL, diagR, column1, column2, column3, row1, row2, row3]
+  for(var i = 0; i < winStates.length; i++){
+    var winSum = []
+    winStates[i].forEach(function(e){
+      winSum.push(e[0])
+    })
+    if(winSum.join("").match(/[x]/g) != null){
+      if(winSum.join("").match(/[x]/g).length === 2 && winSum.indexOf("_") > -1){
+        console.log(winSum)
+        var placeChange = winStates[i][winSum.indexOf("_")][1]
+        $("#" + placeChange).text("o")
+        a[placeChange] = "o"
+        console.log(placeChange)
+        console.log("there's a potetioal block there")
+        console.log(i)
+      }
+    }
+    console.log(winSum)
+    console.log(a)
+  }
+}
+var checkWins = function(){
+  var diagL = [[a[0], 0], [a[4], 4], [a[8], 8]]
+  var diagR =  [[a[2],2], [a[4],4], [a[6],6]]
+  var column1 = [[a[0],0], [a[3],3], [a[6],6]]
+  var column2 = [[a[1],1], [a[4],4], [a[7],7]]
+  var column3 = [[a[2],2], [a[5],5], [a[8],8]]
+  var row1 = [[a[0],0], [a[1],1], [a[2],2]]
+  var row2 = [[a[3],3], [a[4],4], [a[5],5]]
+  var row3 = [[a[6],6], [a[7],7], [a[8],8]]
+  var winStates = [diagL, diagR, column1, column2, column3, row1, row2, row3]
+  for(var i = 0; i < winStates.length; i++){
+    var winSum = []
+    winStates[i].forEach(function(e){
+      winSum.push(e[0])
+    })
+    if(winSum.join("").match(/[o]/g) != null){
+      if(winSum.join("").match(/[o]/g).length === 2 && winSum.indexOf("_") > -1){
+        console.log(winSum)
+        var placeChange = winStates[i][winSum.indexOf("_")][1]
+        $("#" + placeChange).text("o")
+        a[placeChange] = "o"
+        console.log(placeChange)
+        console.log("there's a potetioal win there")
+        console.log(i)
+      }
+    }
+    console.log(winSum)
+    console.log(a)
+  }
+}
 
 var hardPlay = function(){
   var turn = 0
   var diags = [a[0], a[2], a[6], a[8]]
 
-  var checkBlocks = function(){
-    var diagL = [[a[0], 0], [a[4], 4], [a[8], 8]]
-    var diagR =  [[a[2],2], [a[4],4], [a[6],6]]
-    var column1 = [[a[0],0], [a[3],3], [a[6],6]]
-    var column2 = [[a[1],1], [a[4],4], [a[7],7]]
-    var column3 = [[a[2],2], [a[5],5], [a[8],8]]
-    var row1 = [[a[0],0], [a[1],1], [a[2],2]]
-    var row2 = [[a[3],3], [a[4],4], [a[5],5]]
-    var row3 = [[a[6],6], [a[7],7], [a[8],8]]
-    var winStates = [diagL, diagR, column1, column2, column3, row1, row2, row3]
-    for(var i = 0; i < winStates.length; i++){
-      var winSum = []
-      winStates[i].forEach(function(e){
-        winSum.push(e[0])
-      })
-      if(winSum.join("").match(/[x]/g) != null){
-        if(winSum.join("").match(/[x]/g).length === 2){
-          var placeChange = winStates[i][winSum.indexOf("_")][1]
-          $("#" + placeChange).text("o")
-          a[placeChange] = "o"
-          console.log(placeChange)
-          console.log("there's a potetioal block there")
-        }
-      }
-      console.log(winSum)
-      console.log(a)
-    }
-  }
+
 
   $('.box').on('click', function (event) {
     boxId = $(event.target).attr('id')
     console.log(event)
     $("#" + boxId).text("x")
     a[boxId] = "x"
+    gameState(a)
       if(turn === 0){
         turn += 1
          if (a[4]==="x" && diags.indexOf("o") === -1){
@@ -59,19 +85,69 @@ var hardPlay = function(){
          }
       } else if(turn === 1){
         turn += 1
+        checkWins()
+        checkBlocks()
         if (a[0] === "x" && a[8] === "x" || a[2] === "x" && a[6] === "x"){
           a[1] = "o"
           $("#1").text("o")
+        } else if((a[1] === "x" && a[7] === "x")|| (a[3] === "x" && a[5] === "x")){
+          a[2] = "o"
+          $("#2").text("o")
         } else {
-          checkBlocks()
+          if(a[0] === "x" && a[8] === "_"){
+            a[8] = "o"
+            $("#8").text("o")
+          } else if(a[6] === "x" && a[2] === "_"){
+            a[2] = "o"
+            $("#2").text("o")
+          } else if(a[2] === "x" && a[6] === "_"){
+            a[6] = "o"
+            $("#6").text("o")
+          } else if(a[8] === "x" && a[0] === "_"){
+            a[0] = "o"
+            $("#0").text("o")
+          }
         }
       } else if (turn === 2){
+        turn += 1
+        checkWins()
+        if(gameState(a) === "player win" || gameState(a) === "computer win" || gameState(a) === "tie"){
+          return
+        } else {
         checkBlocks()
+        if(gameState(a) === "player win" || gameState(a) === "computer win" || gameState(a) === "tie"){
+          return
+        }
+        gameState(a)
+        }
       } else if (turn === 3){
-        checkBlocks()
-      } else if (turn === 4)
+        turn += 1
+        checkWins()
+        if(gameState(a) === "player win" || gameState(a) === "computer win" || gameState(a) === "tie"){
+          return
+        } else {
+          checkBlocks()
+          if(gameState(a) === "player win" || gameState(a) === "computer win" || gameState(a) === "tie"){
+          return
+        }
+          gameState(a)
+        }
+      } else if (turn === 4){
+        checkWins()
+        if(gameState(a) === "player win" || gameState(a) === "computer win" || gameState(a) === "tie"){
+          return
+        } else {
+          checkBlocks()
+          if(gameState(a) === "player win" || gameState(a) === "computer win" || gameState(a) === "tie"){
+          return
+        }
+          gameState(a)
+        }
+      }
   })
 }
+
+
 function regroup(arr) {
   var z = []
   for (i = 0; i < arr.length; i += 3) {
@@ -106,11 +182,13 @@ function gameState(arr) {
       q[0][2] + q[1][1] + q[2][0] === "ooo") {
         console.log("N0o0ooooo")
         $('span').text("O WInnnss")
+        $('.container')[0].style["opacity"] = "0.6"
         $('#modal').show()
         return "computer win"
       } else if (a.indexOf("_") === -1) {
       console.log("This is a tie!")
       $('span').text("You TIIEEEEE")
+      $('.container')[0].style["opacity"] = "0.6"
       $('#modal').show()
       return "tie"
     }
@@ -134,80 +212,6 @@ function computerPlayEasy(arr) {
 
 }
 
-// var computerPlayHard = function(){
-//   var diags = [a[0], a[2], a[6], a[8]]
-//   var q = regroup(a)
-//
-//
-//   var checkCenter = function(){
-//     if (a[4] === "x" && diags.indexOf("o") === -1){
-//       a[2] = "o"
-//       $("#2").text("o")
-//     }
-//   }
-//
-//   var checkBlocks = function(){
-//     var q = regroup(a)
-//     for(i = 0; i < q.length; i++){
-//       var addIndex = 0
-//       var col = [q[0][i], q[1][i], q[2][i]]
-//       if(i === 1){
-//         addIndex = 3
-//       } else if(i === 2){
-//         addIndex = 5
-//       }
-//       if(q[i].join("").match(/[x]/g).join("") === "xx") {
-//         $("#" + (addIndex + q[i].indexOf("_"))).text("o")
-//         a[addIndex + q[i].indexOf("_")] = "o"
-//
-//       } else if (col.join("").match(/[x]/g).join("") === "xx"){
-//           if (col.indexOf("_") === 0) {
-//             $("#" + i).text("o")
-//             a[i] = "o"
-//           } else if (col.indexOf("_") === 1){
-//             $("#" + (i + 3)).text("o")
-//             a[i + 3] = "o"
-//           } else if (col.indexOf("_") === 2){
-//             $("#" + (i + 6)).text("o")
-//             a[i + 6] = "o"
-//           }
-//       //  } else if ((a[0] + a[4] + a[8]).match(/[x]/g).join("") === "xx") {
-//       //    $("#" + ((a[0] + a[4] + a[8]).indexOf("_"))).text("o")
-//       //    a[(a[0] + a[4] + a[8]).indexOf("_")] = "o"
-//       //  } else if ((a[2] + a[4] + a[6]).match(/[x]/g).join("") === "xx") {
-//       //    $("#" + ((a[2] + a[4] + a[6]).indexOf("_"))).text("o")
-//       //    a[(a[2] + a[4] + a[6]).indexOf("_")] = "o"
-//        }
-//     }
-//   }
-//
-//   var checkCorners = function(){
-//     if(a[0]==="x" && a[8]==="_"){
-//       a[8] = "o"
-//       $("#8").text("o")
-//     } else if(a[8] === "x" && a[0] === "_"){
-//       a[0] = "o"
-//       $("#0").text("o")
-//     } else if(a[2] === "x" && a[6] === "_"){
-//       a[6] = "o"
-//       $("#6").text("o")
-//     } else if(a[6] === "x" && a[2] === "_"){
-//       a[2] = "o"
-//       $("#2").text("o")
-//     }
-//   console.log(a)
-//
-//   }
-//
-//   var playCorners = function(){
-//     if
-//   }
-//
-//   checkCenter()
-//   checkCorners()
-//   checkBlocks()
-//
-// }
 
 
 
@@ -218,6 +222,7 @@ var onePlayerGame = function(arr){
     $("#" + boxId).text("x")
     a[boxId] = "x"
     if(gameState(a) === "player win" || gameState(a) === "computer win" || gameState(a) === "tie"){
+
       return
     } else {
       computerPlayEasy(a)
@@ -240,6 +245,7 @@ var twoPlayerGame = function(arr){
     console.log("gameState: " + a)
     console.log(gameState(a))
     if(gameState(a) === "player win" || gameState(a) === "computer win" || gameState(a) === "tie"){
+      // $('.box').off('click')
       return
     }
     if (turn === "o"){
@@ -266,6 +272,11 @@ var Gamesetup = function Gamesetup(box) {
       $('.box').text("")
       $('#modal').hide()
       $('.container')[0].style["opacity"] = "1"
+
+      if($('input[name=choice]:checked').val() === "3"){
+        $('.box').off('click')
+        hardPlay()
+      }
     })
 
   $('input').on('click', function(e){
@@ -275,11 +286,14 @@ var Gamesetup = function Gamesetup(box) {
     console.log(playChoice)
 
     if (playChoice === "1"){
-
-        twoPlayerGame(box)
-
+      $('.box').off('click')
+        twoPlayerGame(a)
       } else if (playChoice === "2") {
-          hardPlay()
+        $('.box').off('click')
+          onePlayerGame(a)
+      } else if (playChoice === "3"){
+        $('.box').off('click')
+        hardPlay()
       }
     })
   }
